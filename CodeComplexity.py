7874da08f9a5575c89ec636d82f31f70057b3d21
adityawebdevelopment <aditya.bhati05@gmail.com>
2017-12-11 00:40:02 +0000

    commit 2

--- CodeComplexity.py
+++ CodeComplexity.py
@@ -3,15 +3,28 @@ from git import Repo
 import os
 import git
 import shutil
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/CODECOMPLEXITY')
 def Get_repo():
     DIR_NAME = "C:/Users/user/PycharmProjects1"
     REMOTE_URL = "https://github.com/adityabhati12/SCALABLE-COMPUTING---CHAT-SERVER.git"
     Repo.clone_from(REMOTE_URL, DIR_NAME)
     return 'repository cloned'
+
+def create_blocks(list, blocks , directory):
+    size = len(blocks)
+    for block in blocks:
+        if blocks.endswith('.py'):
+            list.append(str(os.path.join(directory, blocks).encode('ascii', 'ignore')))
+
+
+
+
+
 
 
 if __name__ == '__main__':
